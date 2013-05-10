9d8b730d2de6d6ba74530515ea01e88ffc115e4f
popexizhi@gmail.com <popexizhi@gmail.com@182c3415-46d3-f5c6-5c40-5465536c3018>
2013-05-10 09:54:54 +0000

    test AccessData save_STT

---
+++ wiki/test_AccessData.py
@@ -0,0 +1,13 @@
+# -*- coding:UTF8 -*-
+"""
+test AccessData
+"""
+from AccessData import AccessData
+import unittest
+class test_AccessData(unittest.TestCase):
+    def testSaveSTT(self):
+        ''' test Save_STT(PT,STT) '''
+        #<unu> set file
+        #<du> set config，doing
+        #<tri> test result
+        
