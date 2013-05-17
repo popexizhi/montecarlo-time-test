a31df8fee3930640ca9dbc491e5d467843af19f0
popexizhi@gmail.com <popexizhi@gmail.com@182c3415-46d3-f5c6-5c40-5465536c3018>
2013-05-17 09:40:40 +0000

    add AccessData Save_STT & test

--- wiki/AccessData.py
+++ wiki/AccessData.py
@@ -2,6 +2,8 @@
 """
 accessDate in history.list
 """
+import time
+
 class AccessData:
     def __init__(self,filename):
         self.filename=filename
@@ -9,8 +11,29 @@ class AccessData:
         
     def Save_STT(self,PT,STT):
         ''' save STT ,返回id '''
-        id=1
-        return id
+        #<unu> getlast id
+        #<du> add strcon
+        #<tri> save
+
+        #<unu>
+        f=open(self.filename,"r")
+        res=f.readlines()
+        f.close()
+        last=res[len(res)-1]
+        id_last=last.split() 
+        newid=int(id_last[0])+1
+
+        #<du> [next:存储结构后在拼字符串是不是好一点呢？]
+        lit='\t' #使用\t分割
+        PTtime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) #PT估算时间
+        strcon=str(newid)+lit+str(PT)+lit+str(STT)+lit+PTtime+"\n"
+        #<tri>
+        f=open(self.filename,"a")
+        f.writelines(strcon)
+        f.close()
+        
+        
+        return newid
 
     def Save_TT(self,TT,id):
         '''通过id 保存TT '''
@@ -24,4 +47,5 @@ class AccessData:
 
 
 if __name__ == "__main__":
-    m1=AccessData("test_history.list")
+    m1=AccessData("history.list")
+    m1.Save_STT(10,20)
--- wiki/test_AccessData.py
+++ wiki/test_AccessData.py
@@ -4,18 +4,70 @@ test AccessData
 """
 from AccessData import AccessData
 import unittest
+import re
 class test_AccessData(unittest.TestCase):
     def testSaveSTT(self):
-        ''' test Save_STT(PT,STT) '''
+        """test Save_STT(PT,STT)"""
         #<unu> set file
         #<du> set config，doing
         #<tri> test result
+        testfile1="test1.list"
+        testline1=1
+        nextline1=testline1+1
+        PT=10
+        STT=20
+        
+        con="^"+str(nextline1)+'\t'+str(PT)+"\t"+str(STT)+"\t"
+        self.__setfile__(testline1,testfile1) #使用0文件测试
+        a1=AccessData(testfile1)
+        r1=a1.Save_STT(PT,STT)
+        #self.assertEqual(r1,nextline1) #返回行号为next
+        self.__testfileresult__(testfile1,con)
 
-    def __setfile__(self,filename):
-        ''' 设置前置Data使用 '''
+        testfile2="test2.list"
+        testline2=20
+        nextline2=testline2+1
+        PT=10
+        STT=20
+        con="^"+str(nextline2)+'\t'+str(PT)+"\t"+str(STT)+"\t"
+        self.__setfile__(testline2,testfile2) #使用0文件测试
+        a2=AccessData(testfile2)
+        r2=a2.Save_STT(PT,STT)
+        #self.assertEqual(r1,nextline1) #返回行号为next
+        self.__testfileresult__(testfile2,con)        
+        
 
-    def __testresult__(self):
-        ''' 检测条件内容 '''
-        #<unu> test files
-        #<du> test return
-        #<tri> test param changed
+
+    def __setfile__(self,line,filename):
+        '''设置前置Data使用,默认只加入行号 '''
+        f=open(filename,'w')
+        i=1
+        while i<=line:
+            t=str(i)+'\n'
+            #t='11\t10\t20\t'+'\n' #测试匹配使用
+            f.writelines(t)
+            i=i+1
+        f.close()
+
+    def __testfileresult__(self,filename,con):
+        '''检测文件内容结果 '''
+        f=open(filename,'r')
+        res=f.readlines()       #读最后一行内容参见:http://ubuntuforums.org/showthread.php?t=1167061
+        print res
+        if "" == res :
+            last=[]
+        else:
+            last=res[len(res)-1]
+
+        f.close()
+        print last[:]
+
+        #检查内容匹配
+        if not re.search(con,last):
+            print '%r 中不包含 %r' % (last,con)
+        
+        
+
+if __name__=="__main__":
+    
+    unittest.main()
