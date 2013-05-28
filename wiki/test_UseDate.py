183480a60abf0bd0a53d64798cf5574ebce8ec4e
popexizhi@gmail.com <popexizhi@gmail.com@182c3415-46d3-f5c6-5c40-5465536c3018>
2013-05-28 08:11:07 +0000

    TDD 4 testcode test_UseDate.py for testinit is ok

--- wiki/test_UseDate.py
+++ wiki/test_UseDate.py
@@ -9,8 +9,56 @@ class testUseDate(unittest.TestCase):
 
     def testinit(self):
         """test init """
-        pass
+        #<unu> test no list
+        #<du> list < 1000
+        #<tri> list =1000
+        #<kvar> list >1000
 
+        #<unu> [？] 如何可以不创建随机row
+        filename="tUD0.list"
+        a=UseDate.UseDate(filename)
+        #self.__testrow1000(filename)
 
+        #<du>
+        filename="tUD1.list"
+        self.__setfile__(100,filename)
+        a2=UseDate.UseDate(filename)
+        #self.__testrow1000(filename)
+
+        #<tri>
+        filename="tUD2.list"
+        self.__setfile__(1000,filename)
+        a3=UseDate.UseDate(filename)
+        self.__testrow1000(filename)
+
+        #<kvar>
+        filename="tUD3.list"
+        self.__setfile__(1100,filename)
+        a3=UseDate.UseDate(filename)
+        self.__testrow1000(filename)
+        
+        
+        
+    def __testrow1000(self,filename):
+        """test list file 包含 1000条目 """
+        f=open(filename,"r")
+        con=f.readlines()
+        f.close()
+        line=len(con)
+        self.assertEqual(True,line >= 1000 )
+
+    def __setfile__(self,line,filename):
+        """设置前置Data使用,默认只加入行号和TT/PP固定值 """
+        
+        f=open(filename,"w")
+        i=1
+        while i<=line:
+            t=str(i)+'\tPT\tTT\tPT-time\tTT\tTT-time\t20\n'
+            #t='11\t10\t20\t'+'\n' #测试匹配使用
+            f.writelines(t)
+            i=i+1
+        f.close()
+
+        
 if __name__=="__main__":
     unittest.main()
