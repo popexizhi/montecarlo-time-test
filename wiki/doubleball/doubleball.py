3cc17ede886177cd3ffaced131acef44b1fe9c8f
popexizhi@gmail.com <popexizhi@gmail.com@182c3415-46d3-f5c6-5c40-5465536c3018>
2013-08-05 08:48:20 +0000

    脚本备份

--- wiki/doubleball/doubleball.py
+++ wiki/doubleball/doubleball.py
@@ -11,19 +11,42 @@ pope不抱奢望，但真有效果
 import sys
 sys.path.append('..') #引入上级目录中的class 参见:http://xiaofeng1982.blog.163.com/blog/static/315724582010931068584/
 import AcData
+import re
 
 #Define exceptions
 class DoBaError(Exception):pass
+#class DoubleFillError(DoBaError):pass #file must 存在，否则无法读取TT，这里不再做处理
 class PTnumberErr(DoBaError):pass
 class PTidErr(DoBaError):pass
 class PTlessErr(DoBaError):pass
 
 class DoubleBall:
 
+    def __init__(self,filename):
+        """open doubleball_file,err DoubleFillError"""
+        self.filename=filename
+        #get file con
+        f=open(self.filename,"r")
+        self.res=f.readlines()
+        f.close()
+
+        self.lit="\t" #file 分隔符
+
+    def __del__(self):
+        """save file """
+        f=open(self.filename,"w")
+        f.writelines(self.res)
+        f.close()
+        
+
     def SetPT(self,PT,id):
         """set id 中的 PT """
-        pass
-
+        #id 是否存在
+        line=self.__idinfile__(id)
+        #PT 是否已存
+        #保存PT
+        
+        
     def ShowTT(self,id):
         """ show TT """
         pass
@@ -32,7 +55,20 @@ class DoubleBall:
         """get next use """
         pass
 
+    def __idinfile__(self,id):
+        """check id """
+        lines=len(self.res)
+        i=0
+        con="^"+str(id)+self.lit
+        while i<lines:
+            if re.search(con,self.res[i]):
+                return i
+            else:
+                i=i+1
+        raise PTidErr,"%r 不存在" % id
+
 if __name__=="__main__":
-    dB=DoubleBall()
+    dB=DoubleBall("file0.list")
+    dB.SetPT("301","03016")
     
 
--- wiki/doubleball/testdoubleball.py
+++ wiki/doubleball/testdoubleball.py
@@ -8,7 +8,7 @@ import unittest
 
 class testDoubleBall(unittest.TestCase):
     def testSetPT(self):
-        x=doubleball.DoubleBall()
+        x=doubleball.DoubleBall("file.list")
         num="10210301201020"
         id="03001"
         res=x.SetPT(num,id)
@@ -16,21 +16,21 @@ class testDoubleBall(unittest.TestCase):
 
     def testSetPTnum(self):
         "the id 对应的num已经存在 "
-        x=doubleball.DoubleBall()
+        x=doubleball.DoubleBall("file.list")
         num="10210301201020"
         id="03001"
         self.assertRaises(doubleball.PTnumberErr,x.SetPT,num,id)
 
     def testSetPTid(self):
         "id is error"
-        x=doubleball.DoubleBall()
+        x=doubleball.DoubleBall("file.list")
         num="10210301201020"
         id="1"
         self.assertRaises(doubleball.PTidErr,x.SetPT,num,id)
 
     def testSetPTnumless(self):
         "len(num) < 16"
-        x=doubleball.DoubleBall()
+        x=doubleball.DoubleBall("file.list")
         num="10210301201"
         id="1"
         self.assertRaises(doubleball.PTlessErr,x.SetPT,num,id)
